1e8229f16442eef05aa795cb2f05b2ae304b158d
William Woodruff <william@tuffbizz.com>
2018-01-27 13:27:40 -0500

    kbsecret: 1.0.1

--- kbsecret.rb
+++ kbsecret.rb
@@ -1,8 +1,8 @@
 class Kbsecret < Formula
   desc "A secret manager backed by Keybase and KBFS"
   homepage "https://kbsecret.github.io"
-  url "https://github.com/kbsecret/kbsecret/releases/download/1.0.0/kbsecret-1.0.0.tar.gz"
-  sha256 "4559568c28704e47803db3a3eb165e8376e68e73b92b4aaad391268c71e6e5b3"
+  url "https://github.com/kbsecret/kbsecret/releases/download/1.0.1/kbsecret-1.0.1.tar.gz"
+  sha256 "54ab05440ce58fe860df1e4424ac772a284e227f76c62dca635aba292f9c05d3"
 
   def install
     ENV["GEM_HOME"] = libexec
